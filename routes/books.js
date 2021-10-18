42c985e0d1c24f5aa1967a2eb4335db317ab2a38
José Carlos Haza <acremur@gmail.com>
2021-10-19 00:12:41 +0200

    Image Database Updated

--- routes/books.js
+++ routes/books.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 const Book = require('../models/Book')
 const Author = require('../models/Author')
+const imageMimeTypes = ['image/jpeg', 'image/png', 'image/gif']
 
 // All Books Route
 router.get('/', async (req, res) => {
